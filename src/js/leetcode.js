02a006627d0668035ce903333c2e046191549e35
이지호 <leejiho0114@gmail.com>
2025-09-22 09:37:18 +0900

    feat : 파일 저장 경로 수정

--- src/js/leetcode.js
+++ src/js/leetcode.js
@@ -170,7 +170,7 @@ const upload = (
   useDifficultyFolder,
   useLanguageFolder,
 ) => {
-  // const URL = `https://api.github.com/repos/${hook}/contents/${problem}/${filename}`;
+  // const URL = `https://api.github.com/repos/${hook}/contents/LeetCode/${problem}/${filename}`;
   const URL = constructGitHubPath(
     hook,
     basePath,
